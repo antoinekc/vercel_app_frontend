9e62776c5727794a816668d57dc0d6b950975a83
antoinekc <kimkimkimkim0719@gmail.com>
2024-09-20 15:12:06 +0200

    commit

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ fetch('https://vercel-app-backend-gamma.vercel.app/year')
 .then(response => response.json())
  .then(data => {
     console.log('Year received from backend:', data.year);
-   document.querySelector('year').textContent = data.year;
+   document.querySelector('#year').textContent = data.year;
  }); 
